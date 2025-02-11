0b1a13e4529fd508edb0571779479b59d6979f32
Marc Baumholz <marcbaumholz@t-online.de>
2025-02-11 09:42:12 +0100

    Update asset path handling for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,16 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
       },
       output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name][extname]`
+          }
+          return `assets/[name][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       }
     }
   }
